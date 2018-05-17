514b193ae430ca178d01de522a12e6a6d5e81db2
Unknown <benno.driessen@student.hu.nl>
2018-05-17 15:30:49 +0200

    Added line and ellipse

--- Utilities/shapes_lib/stm32_ub_vga_shapes.c
+++ Utilities/shapes_lib/stm32_ub_vga_shapes.c
@@ -52,7 +52,7 @@ const char *UB_VGA_SetPixel(uint16_t xp, uint16_t yp, uint8_t color)
 //--------------------------------------------------------------
 
 //@TODO: Add AA, add width, add errors.
-const char *UB_VGA_drawLine(uint16_t x_start,uint16_t y_start,uint16_t x_stop, uint16_t y_stop, uint8_t width, uint8_t color)
+const char *UB_VGA_drawLine(int16_t x_start,int16_t y_start,int16_t x_stop, int16_t y_stop, uint8_t width, uint8_t color)
 {
 	char *error = "no errors";
 	//checks for out of bound errors
@@ -64,31 +64,98 @@ const char *UB_VGA_drawLine(uint16_t x_start,uint16_t y_start,uint16_t x_stop, u
 	//checks for incorrect line width.
 	if(width < 0 || width > VGA_DISPLAY_Y )
 		error = "incorrect line width";
-
-	//checks for incorrect color
-	if (color < 0)
-		error = "color can't be negative";
-  int16_t dx = x_stop - x_start;
-  int16_t dy = y_stop - y_start;
-  int16_t y;
-  int x;
-  if(dx < dy)
-  {
-	  for(x = y_start ;x <y_stop; x++)
-	  {
-		  y = y_start + dy * (x - x_start)/dx;
-		  error = UB_VGA_SetPixel(x_start, x, color);
-	  }
-  }
-  else
-	  for(x = x_start ;x <x_stop; x++)
-	  {
-		  y = y_start + dy * (x - x_start)/dx;
-		  error = UB_VGA_SetPixel(x, y, color);
-	  }
-  //test return
-  return error;
+	if(width == 0)
+		return;
+		if(abs(y_stop - y_start) < abs(x_stop - x_start))
+		{
+			  y_start -= (width %2 == 0)? width-1/2: (width/2);
+			  y_stop -= (width %2 == 0)? width-1/2: (width/2);
+			while(width > 0)
+			{
+				if(x_start > x_stop)
+					plotLineLow(x_stop, y_stop, x_start, y_start, color);
+				else
+					plotLineLow(x_start, y_start, x_stop, y_stop, color);
+				width--;
+				y_start++;
+				y_stop++;
+			}
+		}
+		else
+		{
+			x_start -= (width %2 == 0)? width-1/2: (width/2);
+			x_stop -= (width %2 == 0)? width-1/2: (width/2);
+			while(width > 0)
+			{
+				if(y_start > y_stop)
+					plotLineHigh(x_stop, y_stop, x_start, y_start, color);
+				else
+					plotLineHigh(x_start, y_start, x_stop, y_stop, color);
+				width--;
+				x_start++;
+				x_stop++;
+			}
+		}
 }
+
+const char plotLineLow(int16_t x_start,int16_t y_start, int16_t x_stop,int16_t y_stop,uint8_t color)
+{
+	int16_t dx = x_stop - x_start;
+	int16_t dy = y_stop - y_start;
+	int16_t yi = 1;
+	int16_t y;
+	int16_t x;
+	if(dy < 0)
+	{
+		yi = -1;
+		dy = -dy;
+	}
+	int16_t D = 2 * dy - dx;
+	y = y_start;
+	for (x = x_start; x<= x_stop; x++)
+	{
+		UB_VGA_SetPixel(x, y, color);
+		if(D > 0)
+
+		{
+			y = y + yi;
+			D = D - 2* dx;
+		}
+
+		D = D + 2 * dy;
+	}
+}
+
+const char plotLineHigh(int16_t x_start,int16_t y_start, int16_t x_stop, int16_t y_stop, uint8_t color)
+{
+	int16_t dx = x_stop - x_start;
+	int16_t dy = y_stop - y_start;
+	int16_t xi = 1;
+	int16_t y;
+	int16_t x;
+	int16_t D;
+	if(dx < 0)
+	{
+		xi = -1;
+		dx = -dx;
+	}
+	D = 2* dx - dy;
+	x = x_start;
+
+	for(y = y_start ; y <= y_stop; y ++)
+	{
+		UB_VGA_SetPixel(x, y, color);
+		if (D > 0)
+		{
+			x = x + xi;
+			D = D - 2 * dy;
+		}
+		D = D + 2 * dx;
+	}
+}
+
+
+
 //--------------------------------------------------------------
 // draws a rectangle from x_lo, y_lo to x_rb, y_rb
 // Important : the last Pixel+1 from every line must be black (don't know why??)
@@ -115,11 +182,14 @@ const char *UB_VGA_drawRectangle(uint16_t x_lo,uint16_t y_lo,uint16_t x_rb, uint
 }
 
 
+
+
+
+
 //--------------------------------------------------------------
 // draws a rectangle from mpx, mpy (mid point x and mid point y) with radius x and radius y.
 // Important : the last Pixel+1 from every line must be black (don't know why??)
 //--------------------------------------------------------------
-//Does not work so well in the y direction. Dont know why.
 const char *UB_VGA_drawEllipse(long xmp,long ymp,long x_radius, long y_radius, uint8_t color)
 {
 	char * error;
@@ -129,87 +199,154 @@ const char *UB_VGA_drawEllipse(long xmp,long ymp,long x_radius, long y_radius, u
 	//checks for incorrect color
 	if (color < 0)
 		error = "color can't be negative";
-	 int x,y;
-	 float d1,d2,dx,dy, rxpsq, rypsq;
-	 x = 0;
-	 y = y_radius;
-	 rxpsq = x_radius * x_radius;
-	 rypsq = y_radius * y_radius;
-	 d1 = (rypsq) - (y_radius * rxpsq + (0.25 * rxpsq));
-	 dx = 2 * rypsq * x;
-	 dy = 2 * rxpsq * y;
 
-	 do
-	 {
-		 setFourPixels(xmp, ymp ,x ,y, color);
-		 if(d1 < 0)
-		 {
-			 x++;
-			 dx = dx + (2 * rypsq);
-			 d1 = d1 + dx + rypsq;
-		 }
-		 else
-		 {
-			x++;
-			y--;
-			dx = dx + (2 * rypsq);
-			dy = dy - (2 * rxpsq);
-			d1 = d1 + dx -dy + rypsq;
-		 }
-	 }while(dx < dy);
-	 do
-	 {
-		 setFourPixels(xmp, ymp ,x ,y, color);
-		 if(d2 > 0)
-		 {
-			 x = x;
-			 y--;
-			 dy = dy - (2 * rxpsq);
-			 d2 = d2- dy + rxpsq;
-		 }
-		 else
-		 {
-			 x++;
-			 y--;
-			 dy = dy - (2 * rxpsq);
-			 dx = dx + (2 * rypsq);
-			 d2 = d2 +dx - dy + rxpsq;
-		 }
-	 }while(y > 0);
+	int hh = y_radius * y_radius;
+	int ww = x_radius * x_radius;
+	int hhww = hh * ww;
+	int x0 = x_radius;
+	int dx = 0;
 
+	// do the horizontal diameter
+	for (int x = -x_radius; x <= x_radius; x++)
+		UB_VGA_SetPixel(xmp + x, ymp, color);
 
-//http://ankurm.com/implementing-midpoint-ellipse-algorithm-in-c/
+	// now do both halves at the same time, away from the diameter
+	for (int y = 1; y <= y_radius; y++)
+	{
+	    int x1 = x0 - (dx - 1);  // try slopes of dx - 1 or more
+	    for ( ; x1 > 0; x1--)
+	        if (x1*x1*hh + y*y*ww <= hhww)
+	            break;
+	    dx = x0 - x1;  // current approximation of the slope
+	    x0 = x1;
+
+	    for (int x = -x0; x <= x0; x++)
+	    {
+	    	UB_VGA_SetPixel(xmp + x, ymp - y, color);
+	    	UB_VGA_SetPixel(xmp + x, ymp + y, color);
+	    }
+	}
 
 return error;
 }
 
 
-//sets four pixels at once for the ellipse function
+//--------------------------------------------------------------
+// draws a triangle from x_one, y_one to x_two, y_two to x_tree, y_tree.
+// Important : the last Pixel+1 from every line must be black (don't know why??)
+//--------------------------------------------------------------
 
-void setFourPixels(uint16_t xs, uint16_t ys, uint16_t x, uint16_t y, uint8_t color)
+const char *UB_VGA_drawTriangle(uint16_t x_one,uint16_t y_one,uint16_t x_two, uint16_t y_two ,uint16_t x_tree, uint16_t y_tree, uint8_t color)
 {
-//	UB_VGA_drawLine(xs + x, ys + y ,xs - x, ys - y,1, color);
-//
-//	UB_VGA_drawLine(xs + x, ys + y ,xs - x, ys + y,1, color);
+	UB_VGA_drawTriangleLine(x_one, y_one, x_two, y_two, color);
 
-
-	///////////////////////////////kinda works///////////////////
-	UB_VGA_drawLine(xs - x ,ys - y ,xs + x, ys + y,1, color);
-	///////////////////////////////kinda works///////////////////
-	UB_VGA_drawLine(xs - x ,ys - y ,xs + x, ys - y,1, color);
-	///////////////////////////////kinda works///////////////////
-	UB_VGA_drawLine(xs + x ,ys - y ,xs + x, ys + y,1, color);
-
-	UB_VGA_drawLine(xs - x ,ys - y ,xs - x, ys + y,1, color);
-
-
-
-
-	UB_VGA_SetPixel(xs + x, ys + y, color);
-	UB_VGA_SetPixel(xs - x, ys - y, color);
-	UB_VGA_SetPixel(xs + x, ys - y, color);
-	UB_VGA_SetPixel(xs - x, ys + y, color);
+	UB_VGA_SetPixel(x_one, y_one, VGA_COL_WHITE);
+	UB_VGA_SetPixel(x_two, y_two, VGA_COL_WHITE);
+	UB_VGA_SetPixel(x_tree, y_tree, VGA_COL_WHITE);
 }
+
+
+
+//--------------------------------------------------------------
+
+// draws a line from x_start, y_start to x_stop, y_stop
+
+// Important : the last Pixel+1 from every line must be black (don't know why??)
+
+//--------------------------------------------------------------
+
+//@TODO: Add AA, add width, add errors.
+const char *UB_VGA_drawTriangleLine(int16_t x_start,int16_t y_start,int16_t x_stop, int16_t y_stop, uint8_t color)
+{
+	char *error = "no errors";
+	//checks for out of bound errors
+	if(x_start< 0 || x_stop < 0 || y_start < 0 || y_stop < 0
+			|| x_start > VGA_DISPLAY_X || x_stop > VGA_DISPLAY_X
+				|| y_start > VGA_DISPLAY_Y || y_stop > VGA_DISPLAY_Y)
+		error = "line out of bounds";
+
+	//checks for incorrect line width.
+	if(width < 0 || width > VGA_DISPLAY_Y )
+		error = "incorrect line width";
+	if(width == 0)
+		return;
+	if(abs(y_stop - y_start) < abs(x_stop - x_start))
+	{
+		if(x_start > x_stop)
+			plotTraingleLineLow(x_stop, y_stop, x_start, y_start, color);
+		else
+			plotTriangleLineLow(x_start, y_start, x_stop, y_stop, color);
+	}
+	else
+	{
+
+		if(y_start > y_stop)
+			plotTriangleLineHigh(x_stop, y_stop, x_start, y_start, color);
+		else
+			plotTriangleLineHigh(x_start, y_start, x_stop, y_stop, color);
+	}
+}
+
+const char plotTriangleLineLow(int16_t start_point, int16_t x_start,int16_t y_start, int16_t x_stop,int16_t y_stop,uint8_t color)
+{
+	int16_t dx = x_stop - x_start;
+	int16_t dy = y_stop - y_start;
+	int16_t yi = 1;
+	int16_t y;
+	int16_t x;
+	if(dy < 0)
+	{
+		yi = -1;
+		dy = -dy;
+	}
+	int16_t D = 2 * dy - dx;
+	y = y_start;
+	for (x = x_start; x<= x_stop; x++)
+	{
+		UB_VGA_SetPixel(x, y, color);
+		if(D > 0)
+
+		{
+			y = y + yi;
+			D = D - 2* dx;
+		}
+
+		D = D + 2 * dy;
+	}
+}
+
+const char plotTriangleLineHigh(int16_t start_point, int16_t x_start,int16_t y_start, int16_t x_stop, int16_t y_stop, uint8_t color)
+{
+	int16_t dx = x_stop - x_start;
+	int16_t dy = y_stop - y_start;
+	int16_t xi = 1;
+	int16_t y;
+	int16_t x;
+	int16_t D;
+	if(dx < 0)
+	{
+		xi = -1;
+		dx = -dx;
+	}
+	D = 2* dx - dy;
+	x = x_start;
+
+	for(y = y_start ; y <= y_stop; y ++)
+	{
+		UB_VGA_SetPixel(x, y, color);
+		if (D > 0)
+		{
+			x = x + xi;
+			D = D - 2 * dy;
+		}
+		D = D + 2 * dx;
+	}
+}
+
+
+
+
+
 
 //--------------------------------------------------------------
 // fill the DMA RAM buffer with one color
--- src/main.c
+++ src/main.c
@@ -29,20 +29,51 @@ int main(void)
 	//demo code to show lines work from every angle in both directions.
 	int i;
 	for(i=10; i < 51; i++)
-		UB_VGA_drawLine(10,i ,50, 50, 3, VGA_COL_RED);
+		UB_VGA_drawLine(10,i ,50, 50, 1, VGA_COL_RED);
+
 	int j;
 		for(j=10; j < 51; j++)
-			UB_VGA_drawLine(50,50 ,90,j, 3, VGA_COL_RED);
+			UB_VGA_drawLine(50,50 ,90,j, 1, VGA_COL_RED);
 
 	UB_VGA_drawRectangle(10,100,90, 50, VGA_COL_RED);
 	UB_VGA_drawRectangle(20,90,80, 60, VGA_COL_BLUE);
 
-	UB_VGA_drawEllipse(150, 150, 40, 80, VGA_COL_BLUE);
-	UB_VGA_drawEllipse(150, 150, 40, 40, VGA_COL_WHITE);
+	//UB_VGA_drawLine(10,9 ,50, 50, 3, VGA_COL_WHITE);
 
-	UB_VGA_drawRectangle(200,190,300, 110, VGA_COL_CYAN);
-	UB_VGA_drawEllipse(250, 150, 40, 40, VGA_COL_WHITE);
 
+	//UB_VGA_drawLine(50,150 ,90,200, 3, VGA_COL_RED);
+
+
+
+//
+//	UB_VGA_drawEllipse(150, 150, 40, 80, VGA_COL_BLUE);
+//	UB_VGA_drawEllipse(50, 150, 40, 40, VGA_COL_WHITE);
+//	UB_VGA_drawEllipse(50, 150, 40, 40, VGA_COL_WHITE);
+//
+//
+//	UB_VGA_drawRectangle(200,190,300, 110, VGA_COL_CYAN);
+//	UB_VGA_drawEllipse(150, 150, 40, 80, VGA_COL_BLUE);
+//
+//	UB_VGA_drawRectangle(200,190,300, 110, VGA_COL_CYAN);
+//	UB_VGA_drawRectangle(20,90,80, 60, VGA_COL_BLUE);
+//
+//
+//	UB_VGA_drawEllipse(150, 150, 40, 80, VGA_COL_BLUE);
+//	//UB_VGA_drawEllipse(150, 150, 80, 80, VGA_COL_BLUE);
+//	UB_VGA_drawEllipse(250, 150, 50, 50, VGA_COL_WHITE);
+
+	UB_VGA_drawTriangle(120, 100, 150, 70, 180, 100, VGA_COL_RED);
+	//UB_VGA_drawTriangle(120, 100, 150, 30, 180, 100, VGA_COL_RED);
+
+	//Testing line width
+	//UB_VGA_drawLine(120,100 ,150, 30, 3, VGA_COL_WHITE);
+//	UB_VGA_drawLine(155,30 ,120, 100, 3, VGA_COL_WHITE);
+//	UB_VGA_drawLine(155,100 ,120, 30, 3, VGA_COL_WHITE);
+//	int k;
+//
+//	UB_VGA_drawLine(200,30 ,200, 100, 3, VGA_COL_WHITE);
+//	UB_VGA_drawLine(120,100 ,150, 100, 3, VGA_COL_WHITE);
+//	UB_VGA_drawLine(140,100 ,150, 30, 3, VGA_COL_WHITE);
 
   while(1)
   {
